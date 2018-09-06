360ef7e58827be1828d454296e0726d0aff4069b
Patrick Son <SonPatrick@users.noreply.github.com>
2018-09-06 10:15:34 -0300

    Limited Upload
    Because the Git Hub upload limit is 25 MB, the upload have only essential archives to project structure. If you want use this project, please, define your project structure like this
    
    libs{Glide, picasso}
      ----app
           |----src
                   |----main{insert Manifest.xml here}
                            |----java {insert java files here}
                            |----res
                                    |----drawable{insert images and side_nav_bar, nav_header_main  archives here}
                                    |----layout {insert layout xml files here}

---
+++ Gif.java
@@ -0,0 +1,55 @@
+package ufopa.edu.br.imagelibs;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+
+public class Gif extends Fragment {
+    private String URL = "https://farmasys.000webhostapp.com/imageLibs/cachorro_gif.gif";
+    private ImageView glideImage;
+    private ImageView picassoImage;
+    private Button loadButton;
+    private View view;
+
+    @SuppressLint("NewApi")
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.root_layout, container, false);
+
+        glideImage = view.findViewById(R.id.glide_imageView);
+        picassoImage = view.findViewById(R.id.picasso_imageView);
+        loadButton = view.findViewById(R.id.button_load);
+
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadImages();
+            }
+        });
+
+        return view;
+    }
+
+    public void loadImages(){
+
+        Glide.with(getActivity())
+                .load(URL)
+                .error(R.color.colorErrorGlide)
+                .into(glideImage);
+
+        Picasso.with(getActivity())
+                .load(URL)
+                .error(R.color.colorErrorPicasso)
+                .into(picassoImage);
+    }
+}
